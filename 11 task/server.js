745d29c4f2ed09ebc57ff833d82c5730af08b17b
Yura Horbachov <71140621+Br1ghTTT@users.noreply.github.com>
2020-10-21 15:48:35 +0300

    Add files via upload

---
+++ 11 task/server.js
@@ -0,0 +1,16 @@
+const http = require("http");
+const os = require('os');
+const path = require('path');
+
+http.createServer (function(request,response){
+    response.writeHead(200, {'Content-Type': 'text/html'});
+    response.write("<h1> System information</h1>");
+    response.write("<p> Current user name: " + os.userInfo().username + "</p> ");
+    response.write("<p> Os type: " + os.type() + "</p>");
+    response.write("<p> System work time: " + os.uptime()+ "</p>");
+    response.write("<p> Current work directory: " + path.dirname(__filename) + "</p>");
+    response.write("<p> Current work directory: " + path.basename(__filename) + "</p>");
+    console.log(os.cpus());
+    console.log(os.userInfo());
+    response.end();
+}).listen(5000);
