3ab556ddcddd116e8b595b688fe3399cc5dd1d5e
Yura Horbachov <71140621+Br1ghTTT@users.noreply.github.com>
2020-10-28 11:07:06 +0200

    Update server.js

--- AJAX/server.js
+++ AJAX/server.js
@@ -4,20 +4,16 @@ var static = require('node-static');
 const file = new static.Server('.' , {
     cache: 0
 });
-
-function accept(req, res){
-    if(req.url == 'info.js'){
-        setTimeout(function(){
+http.createServer (function(req,res){
+    res.writeHead(200, {'Content-Type': 'text/html'}); 
+        if(req === 'GET'){
+            setTimeout(function(){
+                file.serve(req, res);
+            }, 3000);
+            res.end("Your vote is accepted: " + Date());
+        }else{
             file.serve(req, res);
-        }, 3000);
-    }else{
-        file.serve(req, res);
-    }
-}
+        }
+}).listen(5000);
 
-if(!module.parent){
-    http.createServer(accept).listen(8080);
-    console.log("Server is running at http://localhost:8080/")
-}else{
-    exports.accept = accept;
-}
+
